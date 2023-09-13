cc1f65c8081bfaa9451a4532e7c63729f500599c
rwalsh7231 <112423655+rwalsh7231@users.noreply.github.com>
2023-09-13 12:40:21 -0500

    Creation of placeholder foe and player
    Player is able to be controlled with wasd or arrow keys. The Pursuer is set to slowly chase the player and reset game if caught

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //This is possibly placeholder
+
+        //go up
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
+            transform.position += new Vector3(0, 0.5f, 0) * Time.deltaTime;
+        }
+
+        //go down
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
+            transform.position += new Vector3(0, -0.5f, 0) * Time.deltaTime;
+        }
+
+        //go left
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+            transform.position += new Vector3(-0.5f, 0, 0) * Time.deltaTime;
+        }
+
+        //go right
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+            transform.position += new Vector3(0.5f, 0, 0) * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/PursuerScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class PursuerScript : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get player location, and set the Pursuer to go there.
+        Vector3 playerDir = player.position - transform.position;
+        playerDir.Normalize();
+        transform.position += (playerDir * Time.deltaTime)/5;
+    }
+
+    //if the pursuer collides with the player, reset game
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.GetComponent<PlayerMovement>()) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
